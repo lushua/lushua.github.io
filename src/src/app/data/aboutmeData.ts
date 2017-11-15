3e21f9dfddf796ce009cc3fd7b4ab5304c46c29e
Szymon Uglis <szymon.uglis@hotmail.com>
2017-11-15 19:38:48 +0100

    small design changes

--- src/src/app/data/aboutmeData.ts
+++ src/src/app/data/aboutmeData.ts
@@ -1,5 +1,5 @@
 export const ABOUTME: string[] = [
     'Whats up, I\'m Simon, I\'m 17 years old and I\'m in 2st class of high school.',
     'In Programming I\'ve interested for 4 years. The first language I learned was C#.',
-    'I like motorcycles, cycling and gaming for sure.'
+    'I\'m happy owner of Yamaha XJ600, in free time I\'m riding bike with friends and playing games(Dota 2 at most).'
 ];
